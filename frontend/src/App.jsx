87860e2a151808280b8a024d6353917ee0e2e587
Andershsm <10503096@ucn.dk>
2025-03-21 11:44:03 +0100

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css'
+import HomePage from "./pages/HomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+import MovieListPage from "./pages/MovieListPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import { Children } from "react";
+
+const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/movies",
+        element: <MovieListPage />,
+      },
+      {
+        path: "/movies/:name",
+        element: <MovieDetailPage />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,3 @@
+export default function NotFoundPage() {
+    return <h1>Oh no!</h1>;
+  }
